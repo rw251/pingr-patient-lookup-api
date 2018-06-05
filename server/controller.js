6e7655e0e7fbc4fb25e94d2b8f43f9d58ef1ad2c
Richard Williams <richard.williams2@manchester.ac.uk>
2018-06-05 16:17:53 +0100

    only on node 6 on server - no async

--- server/controller.js
+++ server/controller.js
@@ -2,24 +2,18 @@ const Patient = require('./model');
 
 module.exports = {
 
-  lookupAnonId: async (req, res) => {
-    try {
-      const p = await Patient.findOne({ 'characteristics.nhs': req.params.nhsNumber }).lean().exec();
+  lookupAnonId: (req, res) => {
+    Patient.findOne({ 'characteristics.nhs': req.params.nhsNumber }, (err, p) => {
       if (p) res.send({ nhs: +req.params.nhsNumber, anonId: p.patientId });
       else res.send({ errorMsg: 'No patient found' });
-    } catch (e) {
-      res.send({ errorMsg: 'An error occurred.' });
-    }
+    });
   },
 
-  lookupNhs: async (req, res) => {
-    try {
-      const p = await Patient.findOne({ patientId: +req.params.anonId }).lean().exec();
+  lookupNhs: (req, res) => {
+    Patient.findOne({ patientId: +req.params.anonId }, (err, p) => {
       if (p) res.send({ nhs: p.characteristics.nhs, anonId: +req.params.anonId });
       else res.send({ errorMsg: 'No patient found' });
-    } catch (e) {
-      res.send({ errorMsg: 'An error occurred.' });
-    }
+    });
   },
 
 };
