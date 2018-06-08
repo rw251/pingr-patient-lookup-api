70d1d98edfbe5cd1d912f493db95b03b352a7b65
Richard Williams <richard.williams2@manchester.ac.uk>
2018-06-08 14:25:14 +0100

    Return array of matching values instead

--- server/controller.js
+++ server/controller.js
@@ -3,17 +3,42 @@ const Patient = require('./model');
 module.exports = {
 
   lookupAnonId: (req, res) => {
-    Patient.findOne({ 'characteristics.nhs': req.params.nhsNumber }, (err, p) => {
-      if (p) res.send({ nhs: +req.params.nhsNumber, anonId: p.patientId });
-      else res.send({ errorMsg: 'No patient found' });
-    });
+    Patient
+      .find({ 'characteristics.nhs': new RegExp(`^${req.params.nhsNumber}`) })
+      .limit(5)
+      .exec((err, p) => {
+        if (p) {
+          res.send(p.map(v => ({ nhs: v.characteristics.nhs, anonId: v.patientId })));
+        } else res.send([]);
+      });
   },
 
   lookupNhs: (req, res) => {
-    Patient.findOne({ patientId: +req.params.anonId }, (err, p) => {
-      if (p) res.send({ nhs: p.characteristics.nhs, anonId: +req.params.anonId });
-      else res.send({ errorMsg: 'No patient found' });
-    });
+    let anonId = +req.params.anonId;
+    if (anonId < 1) return res.send([]);
+    let diff = 10;
+    const orQuery = [{ patientId: anonId }];
+    anonId *= 10;
+    while (anonId < 100000000) {
+      orQuery.push({
+        $and: [
+          { patientId: { $gte: anonId } },
+          { patientId: { $lt: diff + anonId } }],
+      });
+      anonId *= 10;
+      diff *= 10;
+    }
+
+    console.log(JSON.stringify(orQuery, null, 2));
+
+    return Patient
+      .find({ $or: orQuery })// patientId: +req.params.anonId })
+      .limit(5)
+      .exec((err, p) => {
+        if (p) {
+          res.send(p.map(v => ({ nhs: v.characteristics.nhs, anonId: v.patientId })));
+        } else res.send([]);
+      });
   },
 
 };
