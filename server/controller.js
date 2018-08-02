e3f222180852c9905f5e7102a4144998cef44b46
Richard Williams <richard.williams2@manchester.ac.uk>
2018-08-02 20:48:48 +0100

    Changes to allow unknow nhs numbers

--- server/controller.js
+++ server/controller.js
@@ -7,9 +7,25 @@ module.exports = {
       .find({ 'characteristics.nhs': new RegExp(`^${req.params.nhsNumber}`) })
       .limit(5)
       .exec((err, p) => {
-        if (p) {
-          res.send(p.map(v => ({ nhs: v.characteristics.nhs, anonId: v.patientId })));
-        } else res.send([]);
+        if (err) return res.send({ error: 'An error occurred-lo99. Sorry' });
+        if (p && p.length > 0) {
+          return res.send(p.map(v => ({ nhs: v.characteristics.nhs, anonId: v.patientId })));
+        } else if (RegExp('^[0-9]{10}$').test(req.params.nhsNumber)) {
+          // if 10 digits then let's create a new id
+          // should get a new one from the db but i'm lazy so let's
+          // just make it very unlikely that there'll be a clash
+          // Add 1000000 to ensure higher than those from SIR
+          const id = Math.floor(1000000 + (Math.random() * 8000000));
+          const newPatient = new Patient({
+            patientId: id,
+            characteristics: { nhs: req.params.nhsNumber },
+          });
+          return newPatient.save((err2) => {
+            if (err2) return res.send({ error: 'An error occurred-88j7. Sorry' });
+            return res.send([{ nhs: req.params.nhsNumber, anonId: id }]);
+          });
+        }
+        return res.send([]);
       });
   },
 
@@ -28,8 +44,6 @@ module.exports = {
       anonId *= 10;
       diff *= 10;
     }
-
-    console.log(JSON.stringify(orQuery, null, 2));
 
     return Patient
       .find({ $or: orQuery })// patientId: +req.params.anonId })
